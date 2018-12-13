a5518ffd5c3401b7d50ff9ad466cb2ff9aab9135
Gabriel Sansigolo <gabrielsansigolo@gmail.com>
2018-12-13 16:36:54 -0200

    Fix Project

--- Projeto/Kohonen.R
+++ Projeto/Kohonen.R
@@ -1,11 +1,11 @@
-# satellite image time series package (SITS)
-# example of clustering using self-organizin maps
+##Libraries
 library(sits)
 library(kohonen)
 library(inSitu)
 
-data.tb <- data("cerrado_124K_16classes_6bands")
-data.tb <- cerrado_124K_16classes_6bands
+##Import
+data.tb <- load(file = "/home/sansigolo/Documents/git/Neurocomputing/Projeto/br_mt_1_8K_9classes_6bands.rda")
+data.tb <- br_mt_1_8K_9classes_6bands
 
 #Get time series
 time_series.ts <- sits_values (data.tb, format = "bands_cases_dates")
@@ -28,20 +28,10 @@ koh <-
 #Analyze the mixture between groups and extract informations about confusion matrix
 confusion_by_cluster <- sits_metrics_by_cluster(koh$info_samples)
 confusion_matrix <- confusion_by_cluster$confusion_matrix
+
+#Print matrix
+confusion_matrix
+
+#Plot matrix
 sits_plot_cluster_info(confusion_by_cluster, "Confusion by Cluster")
-
-#Divide groups according to variations
-subgroups <- sits_subgroup(koh)
-
-#Get samples tibble with subgroups
-samples_subgroup <- subgroups$samples_subgroup.tb
-
-#Get neurons and their patterns
-neurons_subgroup <- subgroups$neurons_subgroup.lst
-
-#Number of subgroups for each class
-number_of_subgroup <- lengths(neurons_subgroup)
-
-#Plot subgroups
-sits_plot_subgroups(neurons_subgroup)
 
